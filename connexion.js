9ac7440b13ec269dda66b69257927bbe76c73731
alberic89 <alberic89@gmx.com>
2024-04-30 20:52:34 +0200

    Nitpicking about alphabetizing methods, minor OCD thing

--- connexion.js
+++ connexion.js
@@ -1,7 +1,7 @@
 if (document.URL.includes("cas.eclat-bfc.fr")) {
-	document.getElementById("idp-EDU").click();
-	document.getElementById("button-submit").click();
+    document.getElementById("idp-EDU").click();
+    document.getElementById("button-submit").click();
 };
 if (document.URL.includes("educonnect.education.gouv.fr")) {
-	document.getElementById("bouton_valider").click();
+    document.getElementById("bouton_eleve").click();
 };
